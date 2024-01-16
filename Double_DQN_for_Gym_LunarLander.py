68bdffb22ed82ba94c382b902d5408354487b93e
MohammadAsadolahi <79268727+MohammadAsadolahi@users.noreply.github.com>
2024-01-16 03:14:51 -0800

    Update Double_DQN_for_Gym_LunarLander.py
    The main python file to run the project has been updated to the last version of OpenAI Gym Lunarlander

--- Double_DQN_for_Gym_LunarLander.py
+++ Double_DQN_for_Gym_LunarLander.py
@@ -1,132 +1,68 @@
-# WRITTEN BY MOHAMMAD ASADOLAHI
-# Mohammad.E.Asadolahi@gmail.com
-
-import numpy as np
-import gym
-from keras.layers import Dense, Activation
-from keras.models import Sequential, load_model
-import keras
-import tensorflow as tf
-import numpy as np
-import matplotlib.pyplot as plt
-
-!pip3 install box2d-py
-!pip3 install gym[Box_2D]
-
 
 env = gym.make('LunarLander-v2')
 
-class replayBuffer:
-  def __init__(self,maxSize,stateDim):
-    self.state=np.zeros((maxSize,stateDim))
-    self.action=np.zeros(maxSize,dtype= np.int8)
-    self.reward=np.zeros(maxSize)
-    self.done=np.zeros(maxSize,)
-    self.nextState=np.zeros((maxSize,stateDim))
-    self.maxSize=maxSize
-    self.curser=0
-    self.size=0
-
-  def save(self,state,action,reward,nextState,done):
-    self.state[self.curser]=state
-    self.action[self.curser]=action
-    self.reward[self.curser]=reward
-    self.nextState[self.curser]=nextState
-    self.done[self.curser]=done
-    self.curser=(self.curser+1)%self.maxSize
-    if self.size<self.maxSize:
-      self.size+=1 
-      
-  def sample(self,batchSize):
-    batchSize=min(self.size,batchSize-1)
-    indexes=np.random.choice([i for i in range(self.size-1)],batchSize)
-    return self.state[indexes],self.action[indexes],self.reward[indexes],self.nextState[indexes],self.done[indexes]
-
-class Agent:
-  def __init__(self,stateShape,actionShape,exploreRate,exploreRateDecay,minimumExploreRate,gamma,copyNetsCycle):
-      self.gamma=gamma
-      self.exploreRate=exploreRate
-      self.exploreRateDecay=exploreRateDecay
-      self.minimumExploreRate=minimumExploreRate
-      self.actionShape=actionShape
-      self.memory=replayBuffer(1000000,stateShape)
-      self.model=self.buildModel(stateShape,actionShape)
-      self.model.compile(optimizer='Adam',loss='mse')
-      self.tModel=self.buildModel(stateShape,actionShape)
-      self.tModel.compile(optimizer='Adam',loss='mse')
-      self.learnThreshold=0
-      self.copyNetsCycle=copyNetsCycle
-
-  def buildModel(self,input,output):
-    inputLayer=keras.Input(shape=(input,))
-    layer=Dense(256,activation='relu')(inputLayer)
-    layer=Dense(256,activation='relu')(layer)
-    outputLayer=Dense(output)(layer)
-    model=keras.Model(inputs=inputLayer,outputs=outputLayer)
-    model.compile(optimizer='Adam',loss='mse')
-    return model
-    
-  def getAction(self,state):
-    q=self.model.predict(np.expand_dims(state,axis=0))[0]
-    if np.random.random()<=self.exploreRate:
-      return np.random.choice([i for i in range(env.action_space.n)])
-    else:
-      return np.argmax(q)
-
-  def exploreDecay(self):
-      self.exploreRate=max(self.exploreRate*self.exploreRateDecay,self.minimumExploreRate)
-
-  def saveModel(self,modelName="DoubleDQN_LunarLanderV2.h"):
-      self.model.save_weights(f"{modelName}")
-
-  def loadModel(self,modelName="DoubleDQN_LunarLanderV2.h"):
-      self.model.load_weights(f"{modelName}")
-      self.tModel.set_weights(self.model.get_weights())
-      
-  def learn(self,batchSize=64):
-    if self.memory.size>batchSize:
-      states,actions,rewards,nextStates,done=self.memory.sample(batchSize)
-      qState=self.model.predict(states)
-      qNextState=self.model.predict(nextStates)
-      qNextStateTarget=self.tModel.predict(nextStates)
-      maxActions=np.argmax(qNextState,axis=1)
-      batchIndex = np.arange(batchSize-1, dtype=np.int32)
-      qState[batchIndex,actions]=(rewards+(self.gamma*qNextStateTarget[batchIndex,maxActions.astype(int)]*(1-done)))
-      _=self.model.fit(x=states,y=qState,verbose=0)
-      self.learnThreshold+=1
-      self.exploreDecay()
-      if(self.learnThreshold%self.copyNetsCycle)==0:
-        self.tModel.set_weights(self.model.get_weights())
-        self.saveModel()
-        self.learnThreshold=0
-
 agent=Agent(stateShape=env.observation_space.shape[0],actionShape=env.action_space.n\
             ,exploreRate=1.0,exploreRateDecay=0.9995,minimumExploreRate=0.01,gamma=0.99,copyNetsCycle=100)
-#agent.loadModel()
 
+
+
+# train loop for 200 diffrent episodeds
+
+# to load pretrained model to continue training uncomment the following line (the pretrained wheights are included in the github directory "pretrained_model")
+# agent.loadModel("DoubleDQN_LunarLanderV2.h")
 averageRewards=[]
 totalRewards=[]
 for i in range(1,200):
-  done=False
-  state=env.reset()
+  done = False
+  truncuated = False
+  state,_=env.reset()
   rewards=0
-  while not done:
+  while (not done) and (not truncuated):
     action=agent.getAction(state)
-    nextState,reward,done,info=env.step(action)
+    nextState,reward,done,truncuated,_=env.step(action)
     agent.memory.save(state,action,reward,nextState,int(done))
     rewards+=reward
     state=nextState
     agent.learn(batchSize=64)
   totalRewards.append(rewards)
   averageRewards.append(sum(totalRewards)/len(totalRewards))
-  print(f"episode: {i}   reward: {rewards}  avg so far:{averageRewards[-1]} exploreRate:{agent.exploreRate}")
+  print(f"episode: {i+1}   reward: {rewards}  avg so far:{averageRewards[-1]} exploreRate:{agent.exploreRate}")
 
 plt.title(f'Total Rewards')
-plt.yscale('symlog')
+# plt.yscale('symlog')
 plt.plot(totalRewards)
 plt.savefig("Total Rewards",dpi=200)
+
+
 plt.clf()
 plt.title(f'Average Rewards')
-plt.yscale('symlog')
+# plt.yscale('symlog')
 plt.plot(averageRewards)
 plt.savefig("Average Rewards",dpi=200)
+
+
+
+
+# test loop for 10 diffrent episodeds
+
+# to load pretrained model for evaluation uncomment the following line (the pretrained wheights are included in the github directory "pretrained_model")
+# agent.loadModel("DoubleDQN_LunarLanderV2.h")
+sum_evaluation_rewards=0
+for i in range(0,20):
+  done = False
+  truncuated = False
+  state,_=env.reset()
+  rewards=0
+  while (not done) and (not truncuated):
+    action=agent.getAction(state,True)
+    nextState,reward,done,truncuated,_=env.step(action)
+    rewards+=reward
+    state=nextState
+    # no learning we want to test the model
+  print(f" Test episode: {i+1}   gained reward: {rewards}")
+  sum_evaluation_rewards+=rewards
+print(f"average of evaluation rewards: {sum_evaluation_rewards/20}")
+
+
+
+
